ab9cb8866babaf6d8a49bb81ef658e587c009c2e
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-03-04 00:15:08 +0100

    improve demo

--- demo/index.js
+++ demo/index.js
@@ -3,37 +3,43 @@ const features = [];
 const data = [];
 const recordButton = document.getElementsByClassName("record")[0];
 const predictButton = document.getElementsByClassName("predict")[0];
+const trainingSamplesSection = document.querySelector(".training-samples");
 const state = document.querySelector("h3");
 
 const record = (label) => {
-  state.innerText = "RECORDING...";
-  recordButton.disabled = true;
-  return fetch("/.netlify/functions/record", {
-    method: "POST",
-    body: JSON.stringify({ label }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      state.innerText = "FINISHED";
-      recordButton.disabled = false;
-      if (localStorage.getItem("whereamijs")) {
-        let locationData = localStorage.getItem("whereamijs");
-        let locationDataArray = JSON.parse(locationData);
-        locationData = locationDataArray.push(data);
-        localStorage.setItem("whereamijs", JSON.stringify(locationDataArray));
-      } else {
-        localStorage.setItem("whereamijs", JSON.stringify([data]));
-      }
-    });
+  if (label) {
+    recordButton.disabled = true;
+    recordButton.innerText = "Recording...";
+    recordButton.classList.add("loading");
+    return fetch("/.netlify/functions/record", {
+      method: "POST",
+      body: JSON.stringify({ label }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        recordButton.disabled = false;
+        recordButton.innerText = "Record";
+        if (localStorage.getItem("whereamijs")) {
+          let locationData = localStorage.getItem("whereamijs");
+          let locationDataArray = JSON.parse(locationData);
+          locationData = locationDataArray.push(data);
+          localStorage.setItem("whereamijs", JSON.stringify(locationDataArray));
+        } else {
+          localStorage.setItem("whereamijs", JSON.stringify([data]));
+        }
+        displayLocationsTrained();
+      });
+  }
 };
 
 const predict = () => {
   predictButton.disabled = true;
+  predictButton.innerText = "Predicting...";
+
   let trainingData;
 
   if (localStorage.getItem("whereamijs")) {
     trainingData = localStorage.getItem("whereamijs");
-
     return fetch("/.netlify/functions/predict", {
       method: "POST",
       body: JSON.stringify({ trainingData }),
@@ -41,8 +47,9 @@ const predict = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log("PREDICT DATA", data);
-        state.innerText = data;
+
         predictButton.disabled = false;
+        predictButton.innerText = "Predict";
       })
       .catch((e) => {
         console.log("Error", e);
@@ -55,9 +62,7 @@ const predict = () => {
 recordButton.onclick = () => {
   const sampleLabel = document.getElementsByClassName("label")[0].value;
   labels.push(sampleLabel);
-
   record(sampleLabel);
-  // set button to disabled state to avoid recording twice
 };
 
 predictButton.onclick = () => {
@@ -69,3 +74,24 @@ window.addEventListener("beforeunload", function (event) {
     // localStorage.removeItem("whereamijs");
   }
 });
+
+const displayLocationsTrained = () => {
+  if (localStorage.getItem("whereamijs")) {
+    const dataStored = JSON.parse(localStorage.getItem("whereamijs"));
+    const sectionTitle = document.querySelector(".training-samples h3");
+    sectionTitle.innerText = "Locations trained";
+
+    dataStored.map((d) => {
+      const data = document.createElement("p");
+      data.innerText = d.label;
+      trainingSamplesSection.appendChild(data);
+      trainingSamplesSection.style.display = "block";
+    });
+  } else {
+    const sectionTitle = document.querySelector(".training-samples h3");
+    sectionTitle.innerText = "No location trained";
+  }
+};
+window.onload = () => {
+  displayLocationsTrained();
+};
